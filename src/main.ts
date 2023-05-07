e38ad4c8cf83e6110ff88300f504095bb773601e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-05-07 16:07:48 -0300

    chore: create Dockerfile to make image for development and production

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 
 void bootstrap()
