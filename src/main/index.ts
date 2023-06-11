1a49920730b65e22d0b42ee14311bd3159f1f7f9
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-06-11 17:26:05 -0300

    chore: configure class-validator package

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT || 3000)
 }
 
