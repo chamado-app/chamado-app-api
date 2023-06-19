82210ce242b301895bebedff7976cca0b3d8a1e5
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-06-19 19:37:09 -0300

    test: add skeleton of controller and usecase tests

---
+++ tests/e2e/auth-controller.test.ts
@@ -0,0 +1,13 @@
+describe('AuthController', () => {
+  describe('login', () => {
+    it('should return 422 if e-mail and password is provided')
+
+    it('should return 422 if only e-mail is provided')
+
+    it('should return 422 if only password is provided')
+
+    it('should return 401 if throws UnauthorizedError')
+
+    it('should return 200 if token is returned')
+  })
+})
---
+++ tests/unit/usecases/authenticate-usecase.ts
@@ -0,0 +1,9 @@
+describe('AuthenticateUsecase', () => {
+  it('should throw UnauthorizedError if user is found but password is wrong')
+
+  it('should throw UnauthorizedError if user is not found')
+
+  it('should throw UnauthorizedError if password is wrong')
+
+  it('should return valid token if user is found and password is correct')
+})
