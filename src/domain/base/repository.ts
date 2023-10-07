9f34532d3c1015f57319e3a9fad61a6acf67228e
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-10-07 13:36:51 -0300

    feat: implatement getMany method on repository

--- src/domain/base/repository.ts
+++ src/domain/base/repository.ts
@@ -9,6 +9,6 @@ export abstract class Repository<T extends Entity> {
   // abstract getById(id: number): Observable<T>
   // abstract getAll(): Observable<T[]>
   abstract getOne(filter: Partial<T>): Observable<T>
-  // abstract getMany(filter: Partial<T>): Observable<T[]>
+  abstract getMany(filter: Partial<T>): Observable<T[]>
   abstract delete(filter: Partial<T>): Observable<number>
 }
