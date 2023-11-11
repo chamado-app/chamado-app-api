6ed9396a7222fd8450888edfe85d9297b2525a10
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-11-11 13:15:39 -0300

    fix: sector validation on create user

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -11,7 +11,7 @@ export class UserEntity extends Entity {
   lastName: string
   isActive: boolean
   roles: RoleEntity[]
-  sectors: CategoryEntity[]
+  sectors?: CategoryEntity[]
   tokens?: TokenEntity[]
   createdAt: Date
   updatedAt: Date
