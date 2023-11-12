5b9ff29fa18306582e9eec7b9031b3293cbf7efc
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-11-11 21:34:27 -0300

    feat: add method on controller to update equipment

--- src/presentation/transformers/equipment/index.ts
+++ src/presentation/transformers/equipment/index.ts
@@ -1,3 +1,4 @@
 export * from './create-equipment-input.transformer'
 export * from './show-equipment-input.transformer'
 export * from './show-equipment-output.transformer'
+export * from './update-equipment-input.transformer'
