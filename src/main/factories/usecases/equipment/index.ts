5b2714d2a835f7beb895aab2b46907900de4210f
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2023-11-11 22:19:18 -0300

    feat: add ListEquipmentUsecase to factory composition

--- src/main/factories/usecases/equipment/index.ts
+++ src/main/factories/usecases/equipment/index.ts
@@ -1,4 +1,5 @@
 export * from './create-equipment-usecase.factory'
 export * from './delete-equipment-usecase.factory'
+export * from './list-equipments-usecase.factory'
 export * from './show-equipment-usecase.factory'
 export * from './update-equipment-usecase.factory'
